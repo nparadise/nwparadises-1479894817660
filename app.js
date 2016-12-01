1a255b4e65020839674f0b89d45361ca815338c1
mint3759 <mint3759@gmail.com>
2016-12-02 03:03:34 +0900

    1202 am0303
    src update

--- app.js
+++ app.js
@@ -95,6 +95,29 @@ app.post("/test", function(req, res){
 		}
 	});
 });
+
+var tts_credentials = extend({
+  url: 'https://stream.watsonplatform.net/text-to-speech/api',
+  version: 'v1',
+  username: '6f517a2d-1082-4ced-9567-1c5de272f49b',
+  password: 'ztMYjoTIJkgO',
+}, bluemix.getServiceCreds('text_to_speech'));
+
+// Create the service wrappers
+var textToSpeech = watson.text_to_speech(tts_credentials);
+
+app.get('/synthesize', function(req, res) {
+  var transcript = textToSpeech.synthesize(req.query);
+  transcript.on('response', function(response) {
+    if (req.query.download) {
+      response.headers['content-disposition'] = 'attachment; filename=transcript.ogg';
+    }
+  });
+  transcript.on('error', function(error) {
+    console.log('Synthesize error: ', error)
+  });
+  transcript.pipe(res);
+});
 /*
 var TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
 
@@ -104,50 +127,3 @@ var text_to_speech = watson.text_to_speech({
   password: 'ztMYjoTIJkgO',
   version: 'v1'
 });
-
-// Bootstrap application settings
-require('./config/express')(app);
-
-var textToSpeech = new TextToSpeechV1({
-  // If unspecified here, the TEXT_TO_SPEECH_USERNAME and
-  // TEXT_TO_SPEECH_PASSWORD env properties will be checked
-  // After that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
-  // username: '<username>',
-  // password: '<password>',
-});
-
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.get('/api/synthesize', (req, res, next) => {
-  var transcript = text_to_speech.synthesize(req.query);
-  transcript.on('response', (response) => {
-    if (req.query.download) {
-      if (req.query.accept && req.query.accept === 'audio/wav') {
-        response.headers['content-disposition'] = 'attachment; filename=transcript.wav';
-      } else {
-        response.headers['content-disposition'] = 'attachment; filename=transcript.ogg';
-      }
-    }
-  });
-  transcript.on('error', next);
-  transcript.pipe(res);
-});
-
-// Return the list of voices
-app.get('/api/voices', (req, res, next) => {
-  text_to_speech.voices(null, (error, voices) => {
-    if (error) {
-      return next(error);
-    }
-    res.json(voices);
-  });
-});
-
-// error-handler settings
-require('./config/error-handler')(app);
-
-module.exports = app;
-
-*/
