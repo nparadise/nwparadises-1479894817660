adb53628de0c4a2ddf665c922f30588af7f74ee0
mint3759 <mint3759@gmail.com>
2016-12-04 06:13:58 +0900

    1204 am0613
    send button add

--- public/script.js
+++ public/script.js
@@ -14,6 +14,10 @@ $(document).ready(function(){
 		$('.msg_wrap').slideToggle('slow');
 	});
 
+	$('.msg_click').click(function(){
+		mainRequest();
+	});
+
 	$('.detail_name').click(function(){
 		$('.detail_wrap').slideToggle('slow');
 	});
@@ -30,105 +34,108 @@ $(document).ready(function(){
 	function(e){
 		if (e.keyCode == 13) {
 			e.preventDefault();
-			var msg = $(this).val();
-			$(this).val('');
-			if(msg!='')
-			$('<div class="msg_b"><div class="msg_inb">'+msg+'</div></div>').insertBefore('.msg_push');
-			$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
-			$.ajax({
-				url: "/test",
-				type: "post",
-				contentType: "application/x-www-form-urlencoded",
-				dataType: "text",
-				data: {
-					input_sentence: msg,
-					cur_context: JSON.stringify(context),
-				},
-				beforeSend: function(data) {
-					jQuery("#loading_image").fadeIn();		//show loading image
-				},
-				success: function (data) {
-					var parsedData = JSON.parse(data);
-					context = parsedData.context;			// context passed by watson
-					if (allFoods.length === 0) {
-						allFoods = context.allFoods;		// variable to save every foods
-						console.log(context.allFoods);
+			mainRequest();
+		}
+	});
+
+	var mainRequest = function() {
+		var msg = $('textarea').val();
+		$('textarea').val('');
+		if(msg!='')
+		$('<div class="msg_b"><div class="msg_inb">'+msg+'</div></div>').insertBefore('.msg_push');
+		$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
+		$.ajax({
+			url: "/test",
+			type: "post",
+			contentType: "application/x-www-form-urlencoded",
+			dataType: "text",
+			data: {
+				input_sentence: msg,
+				cur_context: JSON.stringify(context),
+			},
+			beforeSend: function(data) {
+				jQuery("#loading_image").fadeIn();		//show loading image
+			},
+			success: function (data) {
+				var parsedData = JSON.parse(data);
+				context = parsedData.context;			// context passed by watson
+				if (allFoods.length === 0) {
+					allFoods = context.allFoods;		// variable to save every foods
+					console.log(context.allFoods);
+				}
+				console.log(context.angry);
+				context.allFoods = true;				// to notify server that we already have information about every foods
+				output = parsedData.output.text[0];		// watson answer
+				jQuery("#loading_image").fadeOut();		// loading image disappear
+
+				if (context.main === true) {
+					if (context.hasOwnProperty('except')) {
+						exception.push(context.except);		// add ingredients to except
+						delete context.except;
 					}
-					console.log(context.angry);
-					context.allFoods = true;				// to notify server that we already have information about every foods
-					output = parsedData.output.text[0];		// watson answer
-					jQuery("#loading_image").fadeOut();		// loading image disappear
-
-					if (context.main === true) {
-						if (context.hasOwnProperty('except')) {
-							exception.push(context.except);		// add ingredients to except
-							delete context.except;
-						}
-						if (!context.hasOwnProperty('foodnames')) {
-							$('<audio src="/api/synthesize?text=' + output + '&voice=en-US_AllisonVoice" autoplay></audio>').insertBefore('.msg_push');
-							$('<div class="msg_a"><div class="chefPic"></div><div class="msg_ina">'+output+'</div></div>').insertBefore('.msg_push');
-							$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
-						}
-
-						// if next flag is true, show next 4 foods
-						if (context.hasOwnProperty('next') && context.next === true) {
-							printExamples();
-							delete context.next;
-							console.log(context);
-						} else if (context.hasOwnProperty('foodnames')) {	// if user choose food, show that food
-							console.log(context.foodnames);
-							var lookingfor = context.foodnames;		// food to find
-							var checkAppear = false;				// flag to check wether food is in list or not
-							for (var it in nowShowing) {
-
-								for (var j = 0; j < 4; j++) {
-									$("#food_" + j).html("");
-								}
-								if (nowShowing[it].name == lookingfor) { // check food appear
-									checkAppear = true;	
-
-									$('<audio src="/api/synthesize?text=Here+is+the+recipe+of+' + lookingfor + '&voice=en-US_AllisonVoice" autoplay></audio>').insertBefore('.msg_push');	// read the Watson's response
-									$('<div class="msg_a"><div class="chefPic"></div><div class="chefPic"></div><div class="msg_ina">Here is the recipe of ' + lookingfor + '.</div></div>').insertBefore('.msg_push');	// print the Watson's response
-
-									var ratings = nowShowing[it].rating * 20;
-									// show food information
-									var htmlString = '<div class="detail"><div class="detail_name">' + nowShowing[it].name + '</div><div class="detail_wrap">' +
-													'<div class="detail_pic"><img src="' + nowShowing[it].image + '" alt="' + nowShowing[it].name + '" width="380" height="290"/></div>' + 
-													'<div class="detail_right"><div class="detail_rightText"><table><tr><td>cook time: </td><td>' + nowShowing[it].cook_time + ' m</td></tr>' +
-													'<tr><td>rating: </td><td><div class="detail_star"><p class="detail_star2" style="width: ' + ratings + '%;"</p></div></td></tr></table>' +
-													'comment: ' + nowShowing[it].comment + '</table></div><table border="1" class="table_dri">' +
-													'<th colspan="3">Nutrition</th><tr><td></td><td>Quantity</td><td>D.R.I</td></tr>' + 
-													'<tr><td>Calories</td><td>' + nowShowing[it].nutrition.calories + 'kcal</td><td>' + nowShowing[it].nutrition2.calories + '</td></tr>' + 
-													'<tr><td>Fat</td><td>' + nowShowing[it].nutrition.fat + 'g</td><td>' + nowShowing[it].nutrition2.fat + '</td></tr>' + 
-													'<tr><td>Carbs</td><td>' + nowShowing[it].nutrition.carbohydrate + 'g</td><td>' + nowShowing[it].nutrition2.carbohydrate + '</td></tr>' + 
-													'<tr><td>Protein</td><td>' + nowShowing[it].nutrition.protein + 'g</td><td>' + nowShowing[it].nutrition2.protein + '</td></tr>' + 
-													'<tr><td>Cholesterol</td><td>' + nowShowing[it].nutrition.cholesterol + 'mg</td><td>' + nowShowing[it].nutrition2.cholesterol + '</td></tr>' + 
-													'<tr><td>Sodium</td><td>' + nowShowing[it].nutrition.sodium + 'mg</td><td>' + nowShowing[it].nutrition2.sodium + '</td></tr></table></div>' +
-									    			'<div class="detail_bottom"><div class="detail_iT detail_bT">Ingredient</div>' +
-									    			'<div class="detail_i detail_b">' + nowShowing[it].ingredients + '</div><div style="padding: 5px"></div>' +
-									      			'<div class="detail_dT detail_bT">Directions</div><div class="detail_d detail_b">' + nowShowing[it].directions + '</div></div></div></div>';
-									$(htmlString).insertBefore('.msg_push');
-									$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
-								}
-							}
-							if (!checkAppear) { // if food does not appear, show message
-								$('<div class="msg_a"><div class="chefPic"></div><div class="msg_ina">There is no ' + lookingfor + ' in the list.</div></div>').insertBefore('.msg_push');
-								$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
-							}
-							delete context.foodnames;
-						} else {
-							afterGetContext();
-						}
-					} else {
+					if (!context.hasOwnProperty('foodnames')) {
 						$('<audio src="/api/synthesize?text=' + output + '&voice=en-US_AllisonVoice" autoplay></audio>').insertBefore('.msg_push');
 						$('<div class="msg_a"><div class="chefPic"></div><div class="msg_ina">'+output+'</div></div>').insertBefore('.msg_push');
 						$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
 					}
-				}
-			});
-		}
-	});
 
+					// if next flag is true, show next 4 foods
+					if (context.hasOwnProperty('next') && context.next === true) {
+						printExamples();
+						delete context.next;
+						console.log(context);
+					} else if (context.hasOwnProperty('foodnames')) {	// if user choose food, show that food
+						console.log(context.foodnames);
+						var lookingfor = context.foodnames;		// food to find
+						var checkAppear = false;				// flag to check wether food is in list or not
+						for (var it in nowShowing) {
+
+							for (var j = 0; j < 4; j++) {
+								$("#food_" + j).html("");
+							}
+							if (nowShowing[it].name == lookingfor) { // check food appear
+								checkAppear = true;	
+
+								$('<audio src="/api/synthesize?text=Here+is+the+recipe+of+' + lookingfor + '&voice=en-US_AllisonVoice" autoplay></audio>').insertBefore('.msg_push');	// read the Watson's response
+								$('<div class="msg_a"><div class="chefPic"></div><div class="chefPic"></div><div class="msg_ina">Here is the recipe of ' + lookingfor + '.</div></div>').insertBefore('.msg_push');	// print the Watson's response
+
+								var ratings = nowShowing[it].rating * 20;
+								// show food information
+								var htmlString = '<div class="detail"><div class="detail_name">' + nowShowing[it].name + '</div><div class="detail_wrap">' +
+												'<div class="detail_pic"><img src="' + nowShowing[it].image + '" alt="' + nowShowing[it].name + '" width="380" height="290"/></div>' + 
+												'<div class="detail_right"><div class="detail_rightText"><table><tr><td>cook time: </td><td>' + nowShowing[it].cook_time + ' m</td></tr>' +
+												'<tr><td>rating: </td><td><div class="detail_star"><p class="detail_star2" style="width: ' + ratings + '%;"</p></div></td></tr></table>' +
+												'comment: ' + nowShowing[it].comment + '</table></div><table border="1" class="table_dri">' +
+												'<th colspan="3">Nutrition</th><tr><td></td><td>Quantity</td><td>D.R.I</td></tr>' + 
+												'<tr><td>Calories</td><td>' + nowShowing[it].nutrition.calories + 'kcal</td><td>' + nowShowing[it].nutrition2.calories + '</td></tr>' + 
+												'<tr><td>Fat</td><td>' + nowShowing[it].nutrition.fat + 'g</td><td>' + nowShowing[it].nutrition2.fat + '</td></tr>' + 
+												'<tr><td>Carbs</td><td>' + nowShowing[it].nutrition.carbohydrate + 'g</td><td>' + nowShowing[it].nutrition2.carbohydrate + '</td></tr>' + 
+												'<tr><td>Protein</td><td>' + nowShowing[it].nutrition.protein + 'g</td><td>' + nowShowing[it].nutrition2.protein + '</td></tr>' + 
+												'<tr><td>Cholesterol</td><td>' + nowShowing[it].nutrition.cholesterol + 'mg</td><td>' + nowShowing[it].nutrition2.cholesterol + '</td></tr>' + 
+												'<tr><td>Sodium</td><td>' + nowShowing[it].nutrition.sodium + 'mg</td><td>' + nowShowing[it].nutrition2.sodium + '</td></tr></table></div>' +
+								    			'<div class="detail_bottom"><div class="detail_iT detail_bT">Ingredient</div>' +
+								    			'<div class="detail_i detail_b">' + nowShowing[it].ingredients + '</div><div style="padding: 5px"></div>' +
+								      			'<div class="detail_dT detail_bT">Directions</div><div class="detail_d detail_b">' + nowShowing[it].directions + '</div></div></div></div>';
+								$(htmlString).insertBefore('.msg_push');
+								$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
+							}
+						}
+						if (!checkAppear) { // if food does not appear, show message
+							$('<div class="msg_a"><div class="chefPic"></div><div class="msg_ina">There is no ' + lookingfor + ' in the list.</div></div>').insertBefore('.msg_push');
+							$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
+						}
+						delete context.foodnames;
+					} else {
+						afterGetContext();
+					}
+				} else {
+					$('<audio src="/api/synthesize?text=' + output + '&voice=en-US_AllisonVoice" autoplay></audio>').insertBefore('.msg_push');
+					$('<div class="msg_a"><div class="chefPic"></div><div class="msg_ina">'+output+'</div></div>').insertBefore('.msg_push');
+					$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
+				}
+			}
+		});
+	}
 	// function prints 4 examples of foods
 	var printExamples = function() {
 		var j, 				// iterator to iterate 4 times
@@ -255,6 +262,6 @@ $(document).ready(function(){
 		}
 		printExamples();	// after get list, print list
 	}
-
+	
+	mainRequest();
 });
-
